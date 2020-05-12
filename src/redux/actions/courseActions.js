7d5856e621f076defd9bf728086e8d30c28a4767
Teatoller <steven.otieno1@gmail.com>
2020-05-12 14:06:03 +0300

    (middleware): Implement code restructure
    - restructure coursepage
    - install thunk
    - add thunk in store middleware
    - add loadcourses thunk in action file
    - add update constants
    - update reducer with load course action
    - load course component when page mounts
    - create courselist component
    - refactor coursepage
    
    [Finishes #011]

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,5 +1,25 @@
 import * as types from "../constants/courseActionTypes";
+import * as courseApi from "../../api/courseApi";
 
+// action creator
 export function createCourse(course) {
   return { type: types.CREATE_COURSE, course: course };
+}
+
+// thunk
+export function loadCourseSuccess(courses) {
+  return { type: types.LOAD_COURSES_SUCCESS, courses: courses };
+}
+
+export function loadCourses() {
+  return (dispatch) => {
+    return courseApi
+      .getCourses()
+      .then((courses) => {
+        dispatch(loadCourseSuccess(courses));
+      })
+      .catch((error) => {
+        throw error;
+      });
+  };
 }
