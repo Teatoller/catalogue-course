428874efb985d25746da87353ee4bd26be3ac47a
Teatoller <steven.otieno1@gmail.com>
2020-05-22 19:42:17 +0300

    (redirect): Implement redirect to course page
    - install react router dom
    - add course form redirect button
    - add onclick event
    - add local state
    - redirect to courses
    - add client side validation
    - add error handling
    
    [Finishes #020]

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -5,6 +5,8 @@ import { loadAuthors } from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
 import CourseForm from "./CourseForm";
 import { newCourse } from "../../tools/mockData"
+// import Spinner from "../common/Spinner";
+// import { toast } from "react-toastify";
 
 function ManageCoursePage({
   courseReducer,
@@ -12,24 +14,28 @@ function ManageCoursePage({
   loadAuthors,
   loadCourses,
   saveCourse,
+  history,
   ...props
 }) {
   const [course, setCourse] = useState({ ...props.course });
   const [errors, setErrors] = useState({});
+  const [saving, setSaving] = useState(false);
 
   useEffect(() => {
     if (courseReducer.length === 0) {
       loadCourses().catch((error) => {
-        alert("Loading courseReducer failed" + error);
+        alert("Loading courses failed" + error);
       });
+    } else {
+      setCourse({ ...props.course });
     }
 
     if (authorReducer.length === 0) {
       loadAuthors().catch((error) => {
-        alert("Loading authorReducer failed" + error);
+        alert("Loading authors failed" + error);
       });
     }
-  }, []);
+  }, [authorReducer.length, courseReducer.length, loadAuthors, loadCourses, props.course]);
 
   function handleChange(event) {
     const { name, value } = event.target;
@@ -39,9 +45,32 @@ function ManageCoursePage({
     }));
   }
 
+  function formIsValid() {
+    const { title, authorId, category } = course;
+    const errors = {};
+
+    if (!title) errors.title = "Title is required.";
+    if (!authorId) errors.author = "Author is required";
+    if (!category) errors.category = "Category is required";
+
+    setErrors(errors);
+    // Form is valid if the errors object still has no properties
+    return Object.keys(errors).length === 0;
+  }
+
   function handleSave(event) {
     event.preventDefault();
-    saveCourse(course);
+    if (!formIsValid()) return;
+    setSaving(true);
+    saveCourse(course)
+      .then(() => {
+        // toast.success("Course saved.");
+        history.push("/courses");
+      })
+      .catch((error) => {
+        setSaving(false);
+        setErrors({ onSave: error.message });
+      });
   }
 
   return (
@@ -52,6 +81,7 @@ function ManageCoursePage({
         authors={authorReducer}
         onChange={handleChange}
         onSave={handleSave}
+        saving={saving}
       />
     </div>
   );
@@ -64,11 +94,21 @@ ManageCoursePage.propTypes = {
   loadCourses: PropTypes.func.isRequired,
   loadAuthors: PropTypes.func.isRequired,
   saveCourse: PropTypes.func.isRequired,
+  history: PropTypes.object.isRequired,
 };
 
+export function getCourseBySlug(courses, slug) {
+  return courses.find((course) => course.slug === slug) || null;
+}
+
 function mapStateToProps(state, ownProps) {
+  const slug = ownProps.match.params.slug;
+  const course =
+    slug && state.courseReducer.length > 0
+      ? getCourseBySlug(state.courseReducer, slug)
+      : newCourse;
   return {
-    course: newCourse,
+    course,
     courseReducer: state.courseReducer,
     authorReducer: state.authorReducer,
   };
--- src/components/courses/ManageCoursePagex.js
+++ src/components/courses/ManageCoursePagex.js
@@ -1,15 +1,18 @@
 import React, { useEffect, useState } from "react";
-import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { loadCourses } from "../../redux/actions/courseActions";
-import * as authorActions from "../../redux/actions/authorActions";
-import { newCourse } from "../../tools/mockData";
+import { loadCourses, saveCourse } from "../../redux/actions/courseActions";
+import { loadAuthors } from "../../redux/actions/authorActions";
+import PropTypes from "prop-types";
 import CourseForm from "./CourseForm";
+import { newCourse } from "../../tools/mockData";
 
 function ManageCoursePage({
   courseReducer,
   authorReducer,
+  loadAuthors,
   loadCourses,
+  saveCourse,
+  history,
   ...props
 }) {
   const [course, setCourse] = useState({ ...props.course });
@@ -18,46 +21,76 @@ function ManageCoursePage({
   useEffect(() => {
     if (courseReducer.length === 0) {
       loadCourses().catch((error) => {
-        alert("Loading courses failure" + error);
+        alert("Loading courseReducer failed" + error);
       });
     }
 
     if (authorReducer.length === 0) {
-      this.props.actions.loadAuthors().catch((error) => {
-        alert(`Loading authors failure${error}`);
+      loadAuthors().catch((error) => {
+        alert("Loading authorReducer failed" + error);
       });
     }
   }, []);
 
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setCourse((prevCourse) => ({
+      ...prevCourse,
+      [name]: name === "authorId" ? parseInt(value, 10) : value,
+    }));
+  }
+
+  function handleSave(event) {
+    event.preventDefault();
+    saveCourse(course).then(() => {
+      history.push("/courses");
+    });
+  }
+
   return (
     <div>
-      <CourseForm course={course} error={errors} authors={authorReducer} />
+      <CourseForm
+        course={course}
+        errors={errors}
+        authors={authorReducer}
+        onChange={handleChange}
+        onSave={handleSave}
+      />
     </div>
   );
 }
 
 ManageCoursePage.propTypes = {
   course: PropTypes.object.isRequired,
-  actions: PropTypes.object.isRequired, // 3.
-  courseReducer: PropTypes.array.isRequired,
   authorReducer: PropTypes.array.isRequired,
+  courseReducer: PropTypes.array.isRequired,
+  loadCourses: PropTypes.func.isRequired,
+  loadAuthors: PropTypes.func.isRequired,
+  saveCourse: PropTypes.func.isRequired,
+  history: PropTypes.object.isRequired,
 };
 
+export function getCourseBySlug(courseReducer, slug) {
+  return courseReducer.find((course) => course.slug === slug) || null;
+}
+
 function mapStateToProps(state, ownProps) {
+  const slug = ownProps.match.params.slug;
+  const course =
+    slug && state.courseReducer.length > 0
+      ? getCourseBySlug(state.courseReducer, slug)
+      : newCourse;
   return {
-    Course: newCourse,
-    courseReducer: state.authorReducer,
+    course: course,
+    courseReducer: state.courseReducer,
     authorReducer: state.authorReducer,
   };
 }
 
-function mapDispatchToProps(dispatch) {
-  return {
-    actions: {
-      loadCourses: loadCourses,
-      loadAuthors: authorActions.loadAuthors,
-    },
-  };
-}
+const mapDispatchToProps = {
+  loadCourses,
+  loadAuthors,
+  saveCourse,
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursePage);
